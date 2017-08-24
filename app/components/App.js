9d9f8d196bbef367ab6fd3f9dccb54d7aeb497db
Anton Fefilov <anton.fefilov@macrocoders.com>
2017-08-24 12:14:56 +0400

    create static prototype

--- app/components/App.js
+++ app/components/App.js
@@ -1,11 +1,50 @@
 import React from 'react';
 import { footer } from '../styles/footer.scss';
 
+const companies = [
+  { symbol: 'AAPL', name: 'Apple Inc.', price: '$159.07' },
+  { symbol: 'GOOG', name: 'Alphabet Inc.', price: '$921.93' },
+  { symbol: 'MSFT', name: 'Microsoft Corporation', price: '$72.96' }
+];
+
 const App = () =>
-    <div>
-        <h1>Filter table</h1>
-        <footer className={footer}>
-        </footer>
-    </div>;
+  <div>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <h1>Companies list</h1>
+
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Symbol</th>
+                <th>Company name</th>
+                <th>Price</th>
+                <th />
+              </tr>
+            </thead>
+
+            <tbody>
+              {
+                companies.map((company, index) =>
+                  <tr key={index}>
+                    <td> {company.symbol} </td>
+                    <td> {company.name} </td>
+                    <td> {company.price} </td>
+                    <td> <a href="#">Remove</a> </td>
+                  </tr>
+                )
+              }
+            </tbody>
+          </table>
+
+          <button className="btn btn-primary btn-lg">Add company</button>
+        </div>
+      </div>
+
+      <footer className={footer}>
+      </footer>
+    </div>
+  </div>;
 
 export default App;
